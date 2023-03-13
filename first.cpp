366b37db9edc14a2707cc0bb364b1fed3ea23b80
Lukas Jones <lukas@Lukass-MacBook-Pro.local>
2023-03-12 21:04:14 -0400

    added c++ file

---
+++ first.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+#include <stdio.h>
+#include <map>
+#include <fstream>
+#include <ostream>
+#include <istream>
+#include <string>
+#include <sstream>
+#include <stdlib.h>
+
+
+void init (int argc, char** argv){
+    std::ifstream infile;
+    if(argc !=2){
+        std::cerr << "wrong num of args: " << argc-1 << "please supply one infile name \n";
+        exit(1);
+    }
+
+}
+
+
+int main(int argc, char** argv){
+  fprint("hello world");
+  init(argc,argv); 
+  unsigned int committedInsts=0; 
+  unsigned int fetchIndex=0;
+  while(committedInsts<icount){
+    committedInsts=commit(committedInsts);
+    WB();
+    Issue();
+    Dispatch();
+    Rename();
+    Decode(); 
+    fetchIndex=fetch(fetchIndex);
+    cyclecount++; 
+  }
+  emitOutput(); 
+  return 0;
+}
