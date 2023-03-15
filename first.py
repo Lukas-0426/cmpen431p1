b7b777c2e1fd2093b9f966afb9387f88bf9ea1ab
Lukas-0426 <iLukasjg11@gmail.com>
2023-03-15 18:16:38 -0400

    I HATE THIS

--- first.py
+++ first.py
@@ -1,6 +1,6 @@
 
 
-def fetch()
+def fetch(fetchIndex)
 
 def decode()
 
@@ -8,11 +8,26 @@ def rename()
 
 def commit(c)
 
-def init()
+def init(a, b)
 
+def writeback()
 
+def Issue()
+
+def Dispatch()
 
 def main () 
+	init(argc, argv);
+	uint32 committedInsts=0;	 
 
 	while(committedInsts <icount)
 		committedInsts = commit(committedInsts)	
+		writeback();
+		Issue();
+		Dispatch();
+		rename();
+		decode();
+		fetchIndex = fetch(fetchIndex)
+		cyclecount++;
+	emitOutput();
+	return 0;
